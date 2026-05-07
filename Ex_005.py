32a9ba0a6d9294e3fc5324b61e28e7ee9adcdf6b
Junior Sampaio <juniorsampaiio2311@gmail.com>
2026-05-07 16:39:41 -0300

    Atualizar o Ex_005.py

--- Ex_005.py
+++ Ex_005.py
@@ -1,7 +1,7 @@
 #Solicitação de dados
 nome = input("Digite seu nome: ")
 idade = int(input("Quantos anos você tem? "))
-
+cidade = input("De qual cidade você é?: ")
 #Saida de dados
 if idade >= 18:
     print("Olá", nome, "você é maior de idade, vejo que você tem", idade, "anos")
